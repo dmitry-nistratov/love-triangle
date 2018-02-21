589a69be01b0a3cdef1b526db170017d9f14863c
Dmitry Nistratov <nistratovdmitry1@gmail.com>
2018-02-21 20:33:20 +0300

    'solution'

--- src/index.js
+++ src/index.js
@@ -3,5 +3,38 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  number = 0;
+  for(i=0;i<preferences.length; i++){
+  /*if((preferences[param] != param) && (preferences[param+1] != (param+1)) && (preferences[param+2] != (param+2))){
+    returns = returns + 1;
+    return returns;
+  }*/
+  /*if((preferences[i] == (i+2)) && (preferences[i+1] == (i+3)) && (preferences[i+2] == (i+1))){
+    number = number + 1;
+  }*/
+  /*if((preferences[i] == (i+2)) && (preferences[i+1] == (i+3)) && (preferences[i+2] == (i+1))){
+    number = number + 1;
+  }*/
+
+  if (preferences[i] - 1 <= preferences.length && preferences[i] - 1 != preferences[preferences[i] - 1] - 1){
+    if (preferences[preferences[i] - 1] - 1<= preferences.length && preferences[preferences[i] - 1] - 1 != preferences[preferences[preferences[i] - 1] - 1] - 1){
+      if (preferences[preferences[preferences[i] - 1] - 1] - 1 <= preferences.length && preferences[preferences[preferences[i] - 1] - 1] - 1 != preferences[preferences[preferences[preferences[i] - 1] - 1] - 1] - 1){
+        if (preferences[preferences[preferences[preferences[i] - 1] - 1] - 1] - 1<= preferences.length && preferences[i] - 1 != preferences[preferences[i] - 1] - 1){
+    //      if (preferences[preferences[preferences[preferences[i]]]] <= preferences.length){
+            if(i  == preferences[preferences[preferences[i] -1] - 1] -1)
+            {
+              number = number + 1;
+            }
+          //}
+        }
+      }
+    }
+  }
+
+  /*if(preferences[i]  == preferences[preferences[preferences[preferences[i]]]])
+  {
+    number = number + 1;
+  }*/
+}
+return number / 3;
 };
